ca45fb4be4bc19cde63fedf3b7d56abb84f1a9f8
Maliha Fatima <malihameer@hotmail.com>
2024-05-08 20:26:35 -0700

    added articles

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import './components/Sidebar.css';
+import './components/Article.css'
 import { Sidebar } from './components/Sidebar';
+import Article from './components/Article';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sidebar></Sidebar>
+        
       </header>
+      <div className='content'>
+        <div className='left'>
+        <Sidebar></Sidebar>
+        </div>
+        <div className='right'>
+        <Article></Article>
+        </div>
+      </div>
     </div>
   );
 }
--- lab5/src/components/Article.js
+++ lab5/src/components/Article.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+const Article = () => {
+  const [articles, setArticles] = useState([]);
 
-export const Article = () => {
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
+  const fetchData = () => {
+    const sortBy = document.querySelector('input[name="sortBy"]:checked')?.value || 'viewed';
+    const timeFrame = document.querySelector('input[name="timeFrame"]:checked')?.value || '1';
+    const url = `https://api.nytimes.com/svc/mostpopular/v2/${sortBy}/${timeFrame}.json?api-key=hq8UvsE7d5E45IbmtT2XKu8jkpDHV31t`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.results) {
+          setArticles(data.results.slice(0, 6));
+        }
+      })
+      .catch(error => console.log('Error:', error));
+  };
+
   return (
-    <div>Article</div>
-  )
-}
+    <div className="posts">
+      {articles.map((article, index) => (
+        <div className="post" key={index}>
+          <div className="articleHeader">
+            <div className="articleTitle">{index + 1} {article.title}</div>
+            <div className="articleDate">{article.published_date}</div>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', marginLeft: '10px', marginRight: '10px' }}>
+            <div className="articleImg"><img src={article.media[0]?.['media-metadata'][0]?.url} alt="Article Image" /></div>
+            <div className="articleAbstract">{article.abstract}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Article;
