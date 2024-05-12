50325fd2c7519ede7a211d404864a49c44dcfc21
JoyHaddad <joy4haddad@gmail.com>
2024-05-12 13:53:18 -0700

    make articles display as a grid

--- lab5/src/components/Article.js
+++ lab5/src/components/Article.js
@@ -3,6 +3,8 @@ import { Sidebar } from "./Sidebar";
 
 const Article = () => {
   const [articles, setArticles] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(6);
   const [sortBy, setSortBy] = useState("viewed");
   const [timeFrame, setTimeFrame] = useState("1");
 
@@ -20,13 +22,12 @@ const Article = () => {
 
   const fetchData = () => {
     const url = `https://api.nytimes.com/svc/mostpopular/v2/${sortBy}/${timeFrame}.json?api-key=hq8UvsE7d5E45IbmtT2XKu8jkpDHV31t`;
-
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         if (data.results) {
-          setArticles(data.results.slice(0, 6));
+          setArticles(data.results);
         }
       })
       .catch((error) => console.log("Error:", error));
@@ -42,77 +43,39 @@ const Article = () => {
           onTimeFrameChange={onTimeFrameChange}
         />
       </div>
-      <div className="right">
-        <div className="posts">
-          <div className="left">
-            {articles.map((article, index) => {
-              if (index % 2 === 0) {
-                return (
-                  <div className="post" key={index}>
-                    <div className="articleHeader">
-                      <div className="articleTitle">
-                        {index + 1}. {article.title}
-                      </div>
-                      <div className="articleDate">
-                        {article.published_date}
-                      </div>
-                    </div>
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginLeft: "10px",
-                        marginRight: "10px",
-                      }}
-                    >
-                      <div className="articleImg">
-                        <img
-                          src={article.media[0]?.["media-metadata"][0]?.url}
-                          alt="Article Image"
-                        />
-                      </div>
-                      <div className="articleAbstract">{article.abstract}</div>
-                    </div>
-                  </div>
-                );
-              }
-            })}
+      <div
+        className="articlesGrid"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          gap: "5px",
+        }}
+      >
+        {articles.map((article, index) => (
+          <div className="post" key={index}>
+            <div className="articleHeader">
+              <div className="articleTitle">
+                {index + 1}. {article.title}
+              </div>
+              <div className="articleDate">{article.published_date}</div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                margin: "10px",
+              }}
+            >
+              <div className="articleImg">
+                <img
+                  src={article.media[0]?.["media-metadata"][0]?.url}
+                  alt="Article Image"
+                />
+              </div>
+              <div className="articleAbstract">{article.abstract}</div>
+            </div>
           </div>
-          <div className="right">
-            {articles.map((article, index) => {
-              if (index % 2 !== 0) {
-                return (
-                  <div className="post" key={index}>
-                    <div className="articleHeader">
-                      <div className="articleTitle">
-                        {index + 1}. {article.title}
-                      </div>
-                      <div className="articleDate">
-                        {article.published_date}
-                      </div>
-                    </div>
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginLeft: "10px",
-                        marginRight: "10px",
-                      }}
-                    >
-                      <div className="articleImg">
-                        <img
-                          src={article.media[0]?.["media-metadata"][0]?.url}
-                          alt="Article Image"
-                        />
-                      </div>
-                      <div className="articleAbstract">{article.abstract}</div>
-                    </div>
-                  </div>
-                );
-              }
-            })}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
